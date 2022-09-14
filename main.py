845ba172ecd3ad3adf74f5d48dd83b7e95d513d5
MekuzDev <markonuoha97@gmail.com>
2022-09-14 03:32:05 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import turtle as t
+import random
+
+screen = t.Screen()
+screen.setup(width=500, height=400)
+
+user_bet = screen.textinput('guess the winner', "Who will win the race ?")
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+positions = [-70, -40, -10, 20, 50, 80]
+all_turtle = []
+race_is_on = True
+for t1 in range(0, 6):
+    new_turtle = t.Turtle(shape='turtle')
+    new_turtle.color(colors[t1])
+    new_turtle.penup()
+    new_turtle.goto(x=-240, y=positions[t1])
+    all_turtle.append(new_turtle)
+if user_bet:
+    while race_is_on:
+        for t2 in all_turtle:
+            t2.forward(random.randint(0, 15))
+            if t2.xcor() > 230:
+                race_is_on = False
+                if t2.pencolor() == user_bet:
+                    print(f'hurray you won the race, the {user_bet.title()} turtle came 1st')
+                else:
+                    print(f'ooohw!! you lost the race...')
+
+screen.exitonclick()
+
