ed6ece1ee1922da8913ca699dbd75dde08b40cb3
MekuzDev <markonuoha97@gmail.com>
2022-09-14 11:13:31 +0100

    improving final message

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import random
 screen = t.Screen()
 screen.setup(width=500, height=400)
 
-user_bet = screen.textinput('guess the winner', "Who will win the race ?")
+user_bet = screen.textinput(title="Turtle Race",
+                            prompt="guess the winner (red, orange, yellow, green, blue, purple), Who will "
+                                   "win the race ?")
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 positions = [-70, -40, -10, 20, 50, 80]
 all_turtle = []
@@ -19,12 +21,11 @@ if user_bet:
     while race_is_on:
         for t2 in all_turtle:
             t2.forward(random.randint(0, 15))
-            if t2.xcor() > 230:
+            if t2.xcor() > 240:
                 race_is_on = False
                 if t2.pencolor() == user_bet:
                     print(f'hurray you won the race, the {user_bet.title()} turtle came 1st')
                 else:
-                    print(f'ooohw!! you lost the race...')
+                    print(f'ooh!! you lost the race...,  {colors[all_turtle.index(t2)]} you won the race ')
 
 screen.exitonclick()
-
